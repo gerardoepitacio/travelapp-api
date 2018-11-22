97170f35dd0b3e1fc57e811723f6a1669cb757b0
Gerardo Epitacio <juan.epitacio@qacg.com>
2018-11-22 15:12:14 -0600

    Error handler by response code.

--- routes/user.js
+++ routes/user.js
@@ -6,16 +6,15 @@ router.post('/autenticate', function(req, res) {
     if (req.body.username && req.body.password ) {
         User.findOne({ username: req.body.username, password: req.body.password }, function(err, todos) {
             if (err) throw err;
-
             if (todos) {
                 res.send(todos);
             } else {
-                res.send('Err.login.userNotFound');
+                res.status(401).send('Err.login.userNotFound');
             }
         });
     }
     else {
-        res.send('Err.login.requiredFieldsNotFound');
+        res.status(401).send('Err.login.requiredFieldsNotFound');
     }
 });
 
